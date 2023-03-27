cf8917212fda40cdf9483ce25b3c1c8e44cb9e4b
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-28 00:22:53 +0200

    Update file and add readme

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      
+      <h2> COVID-19 Health Apps</h2>
+      <p>We searched for the mHealth apps using the term “COVID-19</p>
     </div>
   )
 }
